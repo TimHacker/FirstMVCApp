9ed3795aef36ef657222843430f210c18620d65e
Tim Hacker <tim.hacker@trueclarity.co.uk>
2011-12-21 14:10:57 +0000

    changed homepage

--- FirstMVCApp/Controllers/HomeController.cs
+++ FirstMVCApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace FirstMVCApp.Controllers
 	{
 		public ActionResult Index()
 		{
-			ViewBag.Message = "Welcome to ASP.NET MVC!";
+			ViewBag.Message = "Kate Hacker";
 
 			return View();
 		}
