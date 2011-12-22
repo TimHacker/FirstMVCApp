a4d26df6db541e9b901e397b59eb097529835af8
Tim Hacker <tim.hacker@trueclarity.co.uk>
2011-12-22 12:56:30 +0000

    jquery experiments

---
+++ FirstMVCApp/Scripts/MyJQueryTest.js
@@ -0,0 +1,11 @@
+﻿/* File Created: December 22, 2011 */
+
+$(function () {
+	$('#box').click(function () {
+		$(this).animate({
+			"top": "+=300px"
+		}, 4000);
+
+		$(this).animate({ "width": "50px" }, 4000);
+	});
+});
